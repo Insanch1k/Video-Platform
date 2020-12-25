006ec44d6067ae7e46828c197f89da27e47226f9
Anton <avk_2202@meta.ua>
2020-12-25 15:07:08 +0100

    added function for deleting acccount

--- src/Controller/Admin/MainController.php
+++ src/Controller/Admin/MainController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
-* @Route ("/admin")
+ * @Route ("/admin")
  */
 class MainController extends AbstractController
 {
@@ -19,19 +19,19 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('admin/my_profile.html.twig',[
-            'subscription'=>$this->getUser()->getSubscription()
+        return $this->render('admin/my_profile.html.twig', [
+            'subscription' => $this->getUser()->getSubscription()
         ]);
     }
 
     public function getAllCategories(CategoryTreeAdminOptionList $categories,
-                                     $editedCategory=0)
+                                     $editedCategory = 0)
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $categories->getCategoryList($categories->buildTree());
-        return $this->render('admin/_all_categories.html.twig',[
-            'categories'=>$categories,
-            'editedCategory'=>$editedCategory
+        return $this->render('admin/_all_categories.html.twig', [
+            'categories' => $categories,
+            'editedCategory' => $editedCategory
         ]);
     }
 
@@ -49,35 +49,48 @@ class MainController extends AbstractController
      */
     public function videos()
     {
-        if($this->isGranted('ROLE_ADMIN'))
-        {
+        if ($this->isGranted('ROLE_ADMIN')) {
             $videos = $this->getDoctrine()->getRepository(Video::class)->findAll();
-        }
-        else {
+        } else {
             $videos = $this->getUser()->getLikedVideos();
         }
-        return $this->render('admin/videos.html.twig',[
-            'videos'=>$videos
+        return $this->render('admin/videos.html.twig', [
+            'videos' => $videos
         ]);
     }
+
     /**
      * @Route ("/cancel-plan", name="cancel_plan")
      */
     public function cancelPlan()
     {
-       $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
+        $user = $this->getDoctrine()->getRepository(User::class)->find($this->getUser());
 
-       $subscription = $user->getSubscription();
-       $subscription->setValidTo(new \Datetime());
-       $subscription->setPaymentStatus(null);
+        $subscription = $user->getSubscription();
+        $subscription->setValidTo(new \Datetime());
+        $subscription->setPaymentStatus(null);
 
 
-       $entityManager = $this->getDoctrine()->getManager();
-       $entityManager->persist($user);
-       $entityManager->persist($subscription);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->persist($subscription);
 
-       $entityManager->flush();
+        $entityManager->flush();
 
-       return $this->redirectToRoute('admin_main_page');
+        return $this->redirectToRoute('admin_main_page');
+    }
+
+    /**
+     * @Route ("/delete-account", name="delete_account")
+     */
+    public function deleteAccount()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+
+        $em->remove($user);
+        $em->flush();
+        session_destroy();
+        return $this->redirectToRoute('main_page');
     }
 }
